05c747c31a1dc30a154c192dca9d7bf234b9da82
Stepan Usatyuk <usaatyuk@ustk.me>
2019-08-01 21:35:55 +0300

    putCommand api

--- Firmware/EggbotWireless/src/WebAPI.cpp
+++ Firmware/EggbotWireless/src/WebAPI.cpp
@@ -1,4 +1,5 @@
 #include "WebAPI.h"
+#include "QueueManager.h"
 
 ESP8266WebServer server(80);
 
@@ -8,7 +9,13 @@ void WebAPI::handleNotFound() {
     server.send(404, "text/plain", "File Not Found\n\n");
 }
 
+void WebAPI::handlePutCommand() {
+    queueManager.putCommand(server.arg("plain").c_str());
+    server.send(200, "text/plain", String(queueManager.execQueueSize()));
+}
+
 void WebAPI::init() {
+    server.on("/putCommand", HTTP_POST, std::bind(&WebAPI::handlePutCommand, this));
     server.onNotFound(std::bind(&WebAPI::handleNotFound, this));
     server.begin();
 }
