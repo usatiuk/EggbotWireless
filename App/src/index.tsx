27a6887d9259873237631a0dae85af1861b8d016
Stepan Usatiuk <stepanusatiuk@gmail.com>
2019-08-02 21:45:01 +0300

    setup a web app

---
+++ App/src/index.tsx
@@ -0,0 +1,7 @@
+import "normalize.css/normalize.css";
+
+import * as React from "react";
+import { render } from "react-dom";
+import { App } from "~App";
+
+render(<App />, document.getElementById("body"));
