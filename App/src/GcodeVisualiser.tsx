1df9fede9db44bf9c510f9d1179ef8917eb2305c
Stepan Usatiuk <stepanusatiuk@gmail.com>
2019-08-07 11:17:06 +0300

    better web app layout

--- App/src/GcodeVisualiser.tsx
+++ App/src/GcodeVisualiser.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Layer, Rect, Stage, Group, Line } from "react-konva";
-import { parseCommand, CommandType } from "~GcodeParser";
+import { parseCommand, CommandType, Command } from "~GcodeParser";
+import { ShapeConfig } from "konva/types/Shape";
 
 interface IGcodeVisualiserProps {
     gcodeLinesSent: string[];
@@ -11,6 +12,44 @@ const mult = 3;
 const width = 500;
 const height = 500;
 
+interface ILastPos {
+    x: number;
+    y: number;
+    eng: boolean;
+}
+
+function cmdToLine(cmds: string, color: string, lastPos: ILastPos) {
+    const command = parseCommand(cmds);
+    if (command.type === CommandType.G00 || command.type === CommandType.G01) {
+        const line = (
+            <Line
+                points={[
+                    lastPos.x * mult,
+                    height - lastPos.y * mult,
+                    (command.X ? command.X : lastPos.x) * mult,
+                    height - (command.Y ? command.Y : lastPos.y) * mult,
+                ]}
+                stroke={(command.Z ? command.Z : lastPos.eng) ? color : "blue"}
+                strokeWidth={(command.Z ? command.Z : lastPos.eng) ? 1 : 0.5}
+            />
+        );
+        if (command.X) {
+            lastPos.x = command.X;
+        }
+        if (command.Y) {
+            lastPos.y = command.Y;
+        }
+        if (command.Z) {
+            if (command.Z >= 0) {
+                lastPos.eng = false;
+            } else {
+                lastPos.eng = true;
+            }
+        }
+        return line;
+    }
+}
+
 export default class GcodeVisualiser extends React.PureComponent<
     IGcodeVisualiserProps,
     {}
@@ -19,98 +58,20 @@ export default class GcodeVisualiser extends React.PureComponent<
         super(props);
     }
     render() {
-        let lastPos: { x: number; y: number; eng: boolean } = {
+        const { gcodeLinesQueue, gcodeLinesSent } = this.props;
+
+        let lastPos: ILastPos = {
             x: 60,
             y: 0,
             eng: false,
         };
 
-        const { gcodeLinesQueue, gcodeLinesSent } = this.props;
-
         const sentLines = gcodeLinesSent.map(sl => {
-            const command = parseCommand(sl);
-            if (
-                command.type === CommandType.G00 ||
-                command.type === CommandType.G01
-            ) {
-                const line = (
-                    <Line
-                        points={[
-                            lastPos.x * mult,
-                            height - lastPos.y * mult,
-                            (command.X ? command.X : lastPos.x) * mult,
-                            height - (command.Y ? command.Y : lastPos.y) * mult,
-                        ]}
-                        stroke={
-                            (command.Z
-                              ? command.Z
-                              : lastPos.eng)
-                                ? "green"
-                                : "blue"
-                        }
-                        strokeWidth={
-                            (command.Z ? command.Z : lastPos.eng) ? 1 : 0.5
-                        }
-                    />
-                );
-                if (command.X) {
-                    lastPos.x = command.X;
-                }
-                if (command.Y) {
-                    lastPos.y = command.Y;
-                }
-                if (command.Z) {
-                    if (command.Z >= 0) {
-                        lastPos.eng = false;
-                    } else {
-                        lastPos.eng = true;
-                    }
-                }
-                return line;
-            }
+            return cmdToLine(sl, "green", lastPos);
         });
 
         const pendLines = gcodeLinesQueue.map(sl => {
-            const command = parseCommand(sl);
-            if (
-                command.type === CommandType.G00 ||
-                command.type === CommandType.G01
-            ) {
-                const line = (
-                    <Line
-                        points={[
-                            lastPos.x * mult,
-                            height - lastPos.y * mult,
-                            (command.X ? command.X : lastPos.x) * mult,
-                            height - (command.Y ? command.Y : lastPos.y) * mult,
-                        ]}
-                        stroke={
-                            (command.Z
-                              ? command.Z
-                              : lastPos.eng)
-                                ? "black"
-                                : "blue"
-                        }
-                        strokeWidth={
-                            (command.Z ? command.Z : lastPos.eng) ? 1 : 0.5
-                        }
-                    />
-                );
-                if (command.X) {
-                    lastPos.x = command.X;
-                }
-                if (command.Y) {
-                    lastPos.y = command.Y;
-                }
-                if (command.Z) {
-                    if (command.Z >= 0) {
-                        lastPos.eng = false;
-                    } else {
-                        lastPos.eng = true;
-                    }
-                }
-                return line;
-            }
+            return cmdToLine(sl, "black", lastPos);
         });
 
         return (
