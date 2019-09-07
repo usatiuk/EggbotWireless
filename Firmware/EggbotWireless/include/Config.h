50841f2c265f1c0d1c10a35b34d19b8251a21e5e
Stepan Usatyuk <usaatyuk@ustk.me>
2019-09-07 14:36:41 +0300

    return actual battery voltage in status

--- Firmware/EggbotWireless/include/Config.h
+++ Firmware/EggbotWireless/include/Config.h
@@ -4,6 +4,9 @@
 constexpr int pin12v{5};
 constexpr int i2cTimeout{10}, i2cTimeoutTries{10};
 constexpr int lastStsTTL{10};
+
+constexpr float battR1{12}, battR2{2.2};
+
 constexpr unsigned long powerTimeout{20000}, powerStartupDelay{100};
 
 constexpr const char *defSSID{"eggbot"}, *defPass{"eggbotwi"},
