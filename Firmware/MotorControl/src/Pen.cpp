b6349c262310793c047365a77adbb943a8d87ea1
Stepan Usatiuk <stepanusatiuk@gmail.com>
2020-03-19 14:24:18 +0300

    [AVR] smooth servo

--- Firmware/MotorControl/src/Pen.cpp
+++ Firmware/MotorControl/src/Pen.cpp
@@ -8,18 +8,20 @@ Pen::Pen(int pin, int posEngaged, int posDisengaged)
 
 void Pen::engage() {
     if (!engaged) {
-        servo.attach(pin);
-        servo.write(posEngaged);
-        delay(200);
+        for(int i = posDisengaged; i > posEngaged; i--) {
+            servo.write(i);
+            delay(5);
+        }
     }
     engaged = true;
 }
 
 void Pen::disengage() {
     if (engaged) {
-        servo.attach(pin);
-        servo.write(posDisengaged);
-        delay(200);
+        for (int i = posEngaged; i > posDisengaged; i++) {
+            servo.write(i);
+            delay(5);
+        }
     }
     engaged = false;
 }
