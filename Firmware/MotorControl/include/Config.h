dbd82156b2f194492d7da9920b5854037dc3eb00
Stepan Usatyuk <stepus53@gmail.com>
2020-03-20 14:28:53 +0300

    more precise movements

--- Firmware/MotorControl/include/Config.h
+++ Firmware/MotorControl/include/Config.h
@@ -6,6 +6,8 @@ constexpr unsigned int stsUpdDelay{10};
 constexpr float defRPM{2}, defEggLength{60}, defEggDia{45}, xLimit{85};
 constexpr float defYDegPerMM{360 / (PI * defEggDia)},
     defXDegPerMM{xLimit / defEggLength};
-constexpr int adjustDelay{100};
+
+constexpr int timer2Div {8};
+constexpr int ticksPerSecond{F_CPU / (timer2Div * 255)};
 
 #endif
